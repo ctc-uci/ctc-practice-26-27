e7ca43fea988b278d30d77f85f955a491d29f4ae
Yousef <theyousefkhan@gmail.com>
2026-07-30 20:05:56 -0700

    Add projects endpoint

---
+++ server/routes/projects.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const { db } = require("../server/db-pgp");
+const { keysToCamel } = require("../common/utils");
+
+const projectsRouter = express.Router();
+
+projectsRouter.use(express.json());
+
+projectsRouter.get("/", async (req, res) => {
+    try {
+        const projects = await db.any(`
+            SELECT
+                p.npo_id,
+                p.start_year,
+                p.end_year,
+                p.project_leads,
+                n.name AS npo_name
+            FROM yk_project_info p
+            JOIN npo_info n ON p.npo_id = n.id
+            ORDER BY p.id;
+        `);
+
+        res.status(200).json(keysToCamel(projects));
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
