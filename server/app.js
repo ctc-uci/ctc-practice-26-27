21dbfdae70f17737d6b70333cb4ed1d6cd2c6dda
Yousef <theyousefkhan@gmail.com>
2026-07-30 20:22:46 -0700

    fix CORS

--- server/app.js
+++ server/app.js
@@ -13,12 +13,12 @@ schedule.scheduleJob("0 0 0 0 0", () => console.log("Hello Cron Job!")); // FIXM
 const app = express();
 
 const CLIENT_HOSTNAME =
-    process.env.NODE_ENV === "DEVELOPMENT"
+    process.env.NODE_ENV === "development"
         ? `${process.env.DEV_CLIENT_HOSTNAME}:${process.env.DEV_CLIENT_PORT}`
         : process.env.PROD_CLIENT_HOSTNAME;
 
 const SERVER_PORT =
-    (process.env.NODE_ENV === "DEVELOPMENT"
+    (process.env.NODE_ENV === "development"
         ? process.env.DEV_SERVER_PORT
         : process.env.PROD_SERVER_PORT) ?? 3001;
 
