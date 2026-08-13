cc1dd100ac76d85f7a0de62a6c41df127278a427
Ananth Kini <ananthkini1@gmail.com>
2026-08-13 16:11:51 -0700

    feat: adds projects route to get project & npo data, updates seed file, updates app to use new route

---
+++ server/routes/projects.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const { db } = require("../server/db-pgp");
+const { keysToCamel } = require("../common/utils");
+
+const projectsRouter = express.Router();
+
+projectsRouter.use(express.json());
+
+projectsRouter.get("/", async (req, res) => {
+    try {
+        const projects = await db.any(`
+           SELECT p.id, n.name, n.description, p.npo_id, p.start_year, p.end_year, p.project_leads
+           FROM ak_project_info p
+           JOIN npo_info n ON p.npo_id = n.id
+           ORDER BY p.id;
+           `);
+        res.status(200).json(keysToCamel(projects));
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
+
+module.exports = projectsRouter;
--- server/seed.js
+++ server/seed.js
@@ -117,4 +117,4 @@ const seed = async (tableName) => {
     }
 };
 
-seed("FILL IN HERE");
+seed("ak_project_info");
